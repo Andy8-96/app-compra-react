08a8394a0fd4c8119c1c0c44370d407c0585c1ce
Andrea Cabral <andrea.cabral@habitant.es>
2022-10-14 09:21:55 +0200

    añadido primeros pasos de la app

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,21 @@
+import React  from 'react';
+
+import  './ExpenseDate.css';
+
+
+function ExpenseDate(props) {
+
+    const mes = props.date.toLocaleString('es-ES', { month : 'long'});
+    const dia = props.date.toLocaleString('es-ES', { day : '2-digit'});
+    const anio = props.date.getFullYear();
+
+    return(
+        <div className='expense-date'>
+            <div className='expense-date__month'>{mes}</div>
+            <div className='expense-date__year'>{anio}</div>
+            <div className='expense-date__day'>{dia}</div>
+        </div>
+    )
+
+}
+export default ExpenseDate;
